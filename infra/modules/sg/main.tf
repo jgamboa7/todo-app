18dc35b6412cb2539a4fe8d7b59008faf9cf5956
jose.gamboa <root@GEG-S305-IT01.GEG.LOCAL>
2025-05-16 17:51:33 +0100

    alb accepts any any inbound

--- infra/modules/sg/main.tf
+++ infra/modules/sg/main.tf
@@ -11,12 +11,10 @@ resource "aws_security_group" "allow_alb_sg" {
 resource "aws_vpc_security_group_ingress_rule" "alb_to_ecs" {
   security_group_id                 = aws_security_group.allow_alb_sg.id
   referenced_security_group_id      = aws_security_group.alb_sg.id
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-  /* from_port                         = 3000
+  from_port                         = 3000
   to_port                           = 3000
   ip_protocol                       = "tcp"  
-} */
+}
 
 resource "aws_vpc_security_group_egress_rule" "ecs_to_alb" {
   security_group_id = aws_security_group.allow_alb_sg.id
@@ -37,9 +35,7 @@ resource "aws_security_group" "alb_sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
