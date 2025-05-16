e65e77e7a4e372827b22c9dc8e90dccdebbf7723
jose.gamboa <root@GEG-S305-IT01.GEG.LOCAL>
2025-05-16 11:46:37 +0100

    adding bootstrap for the terraform backend to run

--- infra/bootstrap/dynamodb-lock-table-todo-app/main.tf
+++ infra/bootstrap/dynamodb-lock-table-todo-app/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
 resource "aws_dynamodb_table" "terraform-lock-table-todo-app" {
   name           = "terraform-lock-table-todo-app"
   billing_mode   = "PAY_PER_REQUEST"
